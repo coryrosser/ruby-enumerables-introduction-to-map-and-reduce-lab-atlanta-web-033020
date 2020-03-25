af166acadc32b987f3457c2bdb089d179c0954fa
Cory Rosser <coryrosser@gmail.com>
2020-03-25 13:40:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # My Code here....
+
+def map_to_negativize(source_array)
+  i = 0 
+  negative_array = []
+  while i < source_array.length do 
+    negative_array.push() source_array[i] * -1 
+  
+  
+  
+end 
